f56a98fffe3707767462c3d75a595eac23e51c62
jotis <jotisanil123@gmail.com>
2023-05-26 21:20:50 +0530

    project made upto store

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+
+$firstName = $_POST['firstName'];
+$number = $_POST['number'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+$conn = new mysqli('localhost','root','','test');
+if($conn->connect_error){
+    die('Connection Failed : '.$conn->connect_error);
+}
+else{
+
+    $stmt = $conn->prepare("insert into registration(firstName, number, email, password )
+           values(?, ?, ?, ?)");
+    $stmt->bind_param("siss",$firstName, $number, $email, $password);
+    $stmt->execute();
+    echo "Succesfuly Registerd";
+    $stmt->close();
+    $open->close();       
+}
+?>
